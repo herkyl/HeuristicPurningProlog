05993deadd1221c0df649e682ad842208fb34333
Rob Fox <s.herkyl@gmail.com>
2012-04-24 22:56:39 +0300

    init

---
+++ main.pl
@@ -0,0 +1,44 @@
+% el_tree(Root, Distance, List_of_leaves).
+:- dynamic el_tree/3.
+
+el_tree(root, 0, [a, b]).
+el_tree(a, 1, [c, d]).
+el_tree(b, 1, [e]).
+el_tree(c, 2, []).
+el_tree(d, 2, []).
+el_tree(e, 2, [x, y, z, u, v]).
+
+
+% Meetod 1
+static_depth_trim_max_depth(1).
+static_depth_trim(Node, TrimmedChildren):-
+    el_tree(Node, Depth, Children),
+    static_depth_trim_max_depth(MaxDepth),
+    (	Depth < MaxDepth, 
+    	get_same(Children, TrimmedChildren),
+    	!
+   	);
+    empty_list(TrimmedChildren).
+    
+% Meetod 4.1
+static_children_trim_max_children(1).
+static_children_trim(Node, TrimmedChildren):-
+	el_tree(Node, _, Children),
+	static_children_trim_max_children(MaxChildren),
+	length(Children, Count).
+	
+	    
+    
+    
+    
+empty_list([]).
+get_same(A, A).
+
+splice([], _, []).
+splice([H|T], Count, NewList):-
+    ((Count == 0), get_same([H|T], NewList), !);
+    NewCount is Count - 1,
+    splice(T, NewCount, NewList).
+.
+ 
+% Arvamus selle kohta, kas on mõistlik laadida kogu sügavus ära, ja siis otsustada selle järgi
