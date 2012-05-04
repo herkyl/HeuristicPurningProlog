b80148d03aa6b415078d9727a4f40e7b75daad35
Serge H <s.herkyl@gmail.com>
2012-05-04 12:29:08 +0300

    added tree size and value calulcating

--- src/main.pl
+++ src/main.pl
@@ -1,24 +1,32 @@
-% el_tree(Root, Distance, List_of_leaves).
-:- dynamic el_tree/3.
+% tree(Root, Distance, List_of_leaves).
+:- dynamic tree/3.
+:- dynamic pruned_tree/3.
 
-el_tree(root, 0, [[a, 1], [b, 1]]).
-el_tree(a, 1, [[c, 1], [d, 1]]).
-el_tree(b, 1, [[e, 1]]).
-el_tree(c, 2, []).
-el_tree(d, 2, []).
-el_tree(e, 2, [[x, 1], [y, 1], [z, 1], [u, 1], [v, 1]]).
+tree(root, 0, [[a, 1], [b, 1]]).
+tree(a, 1, [[c, 1], [d, 1]]).
+tree(b, 1, [[e, 1]]).
+tree(c, 2, []).
+tree(d, 2, []).
+tree(e, 2, [[x, 1], [y, 1], [z, 1], [u, 1], [v, 2]]).
+
 
 % PEA PREDIKAAT - jooksutab programmi
 prune_main:-
+    retractall(pruned_tree(_, _, _)),
     compile('util.pl'),
     compile('prune.pl'),
-	dfs([[root, 0]]).
+    calculate_tree(Cost, Count),
+    writeln(['Cost:', Cost, 'Count:', Count]),
+	ignore(dfs([[root, 0]])),
+    calculate_pruned_tree(Cost2, Count2),
+    writeln(['Cost:', Cost2, 'Count:', Count2]).
 
 % Depth first search
-dfs([[Child, _]|Rest]):-
+dfs([[Child, Cost]|Rest]):-
 	write(Child), nl,
-	el_tree(Child, Depth, C),
+	tree(Child, Depth, C),
 	prune(C, Depth, Children),
+	assert(pruned_tree([Child, Cost], Depth, Children)),
 	dfs(Children);
 	dfs(Rest).
 
@@ -29,7 +37,24 @@ los([[[Child, _], _]|Rest]):-
 	!.
 los([[Child, _]|_]):-
 	write(Child), nl,
-	el_tree(Child, _, Children),
+	tree(Child, _, Children),
 	los(Children).
 
-
+% Arvutab puu suuruse ja hinna
+calculate_tree(Cost, Count):-
+    findall(X, tree(_, _, X), Nodes),
+    calculate_list_of_children(Nodes, Cost, Count).
+calculate_pruned_tree(Cost, Count):-
+    findall(X, pruned_tree(_, _, X), Nodes),
+    calculate_list_of_children(Nodes, Cost, Count).
+calculate_list_of_children([], 0, 0).
+calculate_list_of_children([List|T], Cost, ChildrenCount):-
+    calculate_children(List, C),
+    calculate_list_of_children(T, C2, CC),
+    length(List, Length),
+    ChildrenCount is CC + Length,
+    Cost is C + C2.
+calculate_children([], 0).
+calculate_children([[_, CC]|T], Cost):-
+    calculate_children(T, TailCost),
+    Cost is CC + TailCost.
